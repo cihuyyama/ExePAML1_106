eea0566dbfeae83381720f865c93d7261bd1b818
cihuyyama <miqbalalhabib@gmail.com>
2023-04-01 20:36:06 +0700

    adding pp from asset

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -15,7 +15,44 @@ class HomePage extends StatelessWidget {
         child: SingleChildScrollView(
           child: Column(
             children: [
-              
+              Row(
+                children: [
+                  Container(
+                    padding: EdgeInsets.only(top: 50, left: 20),
+                    child: RichText(
+                      text: TextSpan(
+                          text: 'Hallo, ',
+                          style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.black),
+                          children: <TextSpan>[
+                            TextSpan(
+                                text: '$name',
+                                style: TextStyle(
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.bold,
+                                    color: Color.fromARGB(255, 42, 154, 237)))
+                          ]),
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(top: 70, left: 190),
+                    width: 60,
+                    height: 60,
+                    decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        border: Border.all(color: Colors.black12, width: 1)),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(50),
+                      child: Image.asset(
+                        'assets/images/pokemon-trainer.png',
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  )
+                ],
+              )
             ],
           ),
         ),

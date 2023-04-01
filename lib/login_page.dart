33ce0819e483f1ef59509abbf51a0d2792d11a96
cihuyyama <miqbalalhabib@gmail.com>
2023-04-01 17:03:29 +0700

    LoginPage well designed

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,16 +1,239 @@
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:pokedex_exe/main.dart';
 
 class LoginPage extends StatefulWidget {
-  const LoginPage({super.key});
+  const LoginPage({
+    Key? key,
+  }) : super(key: key);
 
   @override
   State<LoginPage> createState() => _LoginPageState();
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final _formkey = GlobalKey<FormState>();
+  String? name;
+  String? pass;
+  bool _passwordVisible = false;
+  final passcon = TextEditingController();
+  final namecon = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+        body: SizedBox(
+            width: double.infinity,
+            height: double.infinity,
+            child: Form(
+              key: _formkey,
+              child: SingleChildScrollView(
+                child: Column(children: <Widget>[
+                  Container(
+                    margin: const EdgeInsets.only(
+                        top: 70, bottom: 10, left: 25, right: 25),
+                    child: Image.asset(
+                      'assets/images/pokemon.png',
+                      height: 150,
+                    ),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.only(
+                        top: 10, bottom: 5, left: 5, right: 5),
+                    child: const Text(
+                      "Let's get started.",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 30.0,
+                        fontWeight: FontWeight.bold,
+                        color: Color.fromARGB(255, 255, 219, 5),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.only(
+                        top: 15, bottom: 40, left: 10, right: 10),
+                    child: const Text(
+                      "There's No Sense In Going Out Of\nYour Way To Get Somebody To Like You",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Color.fromARGB(255, 159, 158, 159),
+                      ),
+                    ),
+                  ),
+                  Container(
+                      margin: EdgeInsets.only(top: 15, left: 20),
+                      child: Row(
+                        children: [
+                          const Text(
+                            "Name",
+                            textAlign: TextAlign.left,
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: Color.fromARGB(255, 33, 150, 243)),
+                          ),
+                        ],
+                      )),
+                  Container(
+                    margin: EdgeInsets.only(top: 0, right: 20, left: 20),
+                    child: Column(
+                      children: [
+                        TextFormField(
+                          decoration: InputDecoration(
+                              hintText: 'Enter your Name',
+                              prefixIcon: const Icon(Icons.person)),
+                          validator: (value) {
+                            if (!value!.isEmpty) {
+                              return 'Please enter your Name';
+                            }
+                            return null;
+                          },
+                          onSaved: (value) {
+                            name = value;
+                          },
+                        ),
+                      ],
+                    ),
+                  ),
+                  Container(
+                      margin: EdgeInsets.only(top: 15, left: 20),
+                      child: Row(
+                        children: [
+                          const Text(
+                            "Password",
+                            textAlign: TextAlign.left,
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: Color.fromARGB(255, 33, 150, 243)),
+                          ),
+                        ],
+                      )),
+                  Container(
+                    margin: EdgeInsets.only(top: 0, right: 20, left: 20),
+                    child: Column(
+                      children: [
+                        TextFormField(
+                          validator: (value) {
+                            if (!value!.isEmpty) {
+                              return 'Please enter your password';
+                            }
+                            return null;
+                          },
+                          onSaved: (value) {
+                            pass = value;
+                          },
+                          obscureText: !_passwordVisible,
+                          decoration: InputDecoration(
+                            hintText: 'Enter your Password',
+                            prefixIcon: const Icon(Icons.lock),
+                            suffixIcon: IconButton(
+                              icon: Icon(
+                                _passwordVisible
+                                    ? Icons.visibility_off
+                                    : Icons.visibility,
+                              ),
+                              onPressed: () {
+                                setState(() {
+                                  _passwordVisible = !_passwordVisible;
+                                });
+                              },
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(top: 70),
+                    child: Column(
+                      children: [
+                        ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            primary: Color.fromARGB(255, 255, 219, 5),
+                            onPrimary: Colors.white,
+                            fixedSize: Size(350, 50),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(35)
+                            )
+                          ),
+                          child: const Text(
+                            'Sign In',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 17
+                            ),
+                          ),
+                          onPressed: () {
+                            if (_formkey.currentState!.validate()) {
+                              _formkey.currentState!.save();
+                              // Navigator.push(
+                              //     context,
+                              //     MaterialPageRoute(
+                              //       builder: (context) => HomePage(
+                              //         name: name,
+                              //       ),
+                              //     ));
+                            }
+                          },
+                        )
+                      ],
+                    ),
+                  ),
+
+                  Container(
+                    margin: EdgeInsets.only(top: 15,left: 85),
+                    child: Row(
+                      children: [
+                        Text.rich(
+                          TextSpan(
+                            text: "Don't have an account? ",
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: Color.fromARGB(255, 186, 185, 187)
+                            ),
+                            children: [
+                              TextSpan(
+                                text: " Sign Up",
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  color: Color.fromARGB(255, 246, 201, 27),
+                                ),
+                              )
+                            ]
+                          )
+                        )
+                      ],
+                    ),
+                  )
+
+                  // Container(
+                  //   margin: EdgeInsets.only(top: 15,left: 85),
+                  //   child: Row(
+                  //     children: [
+                  //       const Text(
+                  //         "Don't have an account? ",
+                  //         style: TextStyle(
+                  //           fontWeight: FontWeight.bold,
+                  //           color: Color.fromARGB(255, 186, 185, 187)
+                  //         ),
+                  //       ),
+                  //       const Text(
+                  //         ' Sign Up',
+                          
+                  //         style: TextStyle(
+                  //           fontWeight: FontWeight.bold,
+                  //           color: Color.fromARGB(255, 246, 201, 27),
+                            
+                  //         ),
+                  //       )
+                  //     ],
+                  //   ),
+                  // )
+                ]),
+              ),
+            )));
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -37,28 +37,28 @@ class _IntroPageState extends State<IntroPage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Container(
-              padding: const EdgeInsets.only(top: 30, bottom: 30),
+              margin: const EdgeInsets.only(top: 30, bottom: 30),
               child: const Text(
                 "Pokedex.",
                 style: TextStyle(
                   fontSize: 34.0,
                   fontWeight: FontWeight.bold,
-                  color: Colors.yellow,
+                  color: Color.fromARGB(255, 255, 219, 5),
                 ),
               ),
             ),
             Container(
-              padding: const EdgeInsets.only(top: 20, bottom: 20),
+              margin: const EdgeInsets.only(top: 20, bottom: 20),
               child: const Text(
                 "Welcome to Pokedex. \n You can find your favorite pokemon here",
                 textAlign: TextAlign.center,
                 style: TextStyle(
-                  color: Color.fromARGB(255, 150, 150, 150),
+                  color: Color.fromARGB(255, 159, 158, 159),
                 ),
               ),
             ),
             Container(
-              padding: const EdgeInsets.only(top: 130, bottom: 150),
+              margin: const EdgeInsets.only(top: 130, bottom: 150),
               child: Image.asset(
                 'assets/images/pokeball.png',
                 height: 150,
@@ -70,7 +70,7 @@ class _IntroPageState extends State<IntroPage> {
                   context, MaterialPageRoute(builder: (context) => const LoginPage(),)
                 );
               },
-              backgroundColor: Colors.yellow.shade300,
+              backgroundColor: Color.fromARGB(255, 255, 219, 5),
               child: const Icon(Icons.arrow_forward),
             )
           ],
